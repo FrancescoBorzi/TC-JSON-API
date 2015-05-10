66a4fd5756fd78c881da3a5f09ca40d1ebccb676
ShinDarth <borzifrancesco@gmail.com>
2015-05-10 11:32:25 +0200

    Add character's basic routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -388,6 +388,34 @@ Route::get('/loot/spell/item/{id}', function($id) {
   ->where('id', '[0-9]+');
 
 
+/* Characters */
+
+Route::get('/characters/{guid}', function($guid) {
+  $results = DB::connection('characters')->select("SELECT t1.guid, t1.name, t3.guildid as guildId, t3.name AS guildName, t1.race, t1.gender, t1.level, t1.xp, t1.money, t1.playerBytes, t1.playerBytes2, t1.playerFlags, t1.map, t1.instance_id, t1.online, t1.is_logout_resting, t1.arenaPoints, t1.totalHonorPoints, t1.todayHonorPoints, t1.yesterdayHonorPoints, t1.totalKills, t1.todayKills, t1.yesterdayKills, t1.chosenTitle FROM characters AS t1 LEFT JOIN guild_member AS t2 ON t1.guid = t2.guid LEFT JOIN guild AS t3 ON t2.guildid = t3.guildid WHERE t1.guid = ?", [$guid]);
+
+  return Response::json($results);
+})
+  ->where('guid', '[0-9]+');
+
+Route::get('/characters/{name}', function($name) {
+
+  $name = DB::connection()->getPdo()->quote("%" . $name . "%");
+
+  $query = sprintf("SELECT t1.guid, t1.name, t3.guildid as guildId, t3.name AS guildName, t1.race, t1.gender, t1.level, t1.xp, t1.money, t1.playerBytes, t1.playerBytes2, t1.playerFlags, t1.map, t1.instance_id, t1.online, t1.is_logout_resting, t1.arenaPoints, t1.totalHonorPoints, t1.todayHonorPoints, t1.yesterdayHonorPoints, t1.totalKills, t1.todayKills, t1.yesterdayKills, t1.chosenTitle FROM characters AS t1 LEFT JOIN guild_member AS t2 ON t1.guid = t2.guid LEFT JOIN guild AS t3 ON t2.guildid = t3.guildid WHERE t1.name LIKE %s", $name);
+
+  $results = DB::connection('characters')->select($query);
+
+  return Response::json($results);
+});
+
+Route::get('/online', function() {
+
+  $results = DB::connection('characters')->select("SELECT t1.guid, t1.name, t3.guildid as guildId, t3.name AS guildName, t1.race, t1.gender, t1.level, t1.map, t1.instance_id FROM characters AS t1 LEFT JOIN guild_member AS t2 ON t1.guid = t2.guid LEFT JOIN guild AS t3 ON t2.guildid = t3.guildid WHERE t1.online = 1");
+
+  return Response::json($results);
+});
+
+
 /* Other */
 
 Route::get('/version', function() {
