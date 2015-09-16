4fe4c8b5b6f4061083e9b70e77ce58a7bfec1d60
ShinDarth <borzifrancesco@gmail.com>
2015-09-16 02:48:32 +0200

    Update credits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,8 @@
                 <div class="title">TrinityCore-JSON-API</div>
                 <div class="item">API version: <strong>0.6</strong></div>
                 <div class="item">Database version: <strong><?= $db_version ?></strong></div>
-                <div class="credit">Created by <a target="_blank" href="https://github.com/ShinDarth"><strong>ShinDarth</strong></a></div>
+                <!-- We worked for free to build this software, please do not remove the credit! -->
+                <div class="credit">Created by <a target="_blank" href="https://github.com/ShinDarth"><strong>ShinDarth</strong></a> && <a target="_blank" href="https://github.com/Helias"><strong>Helias</strong></a></div>
             </div>
         </div>
         <a target="_blank" href="https://github.com/ShinDarth/TC-JSON-API"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
