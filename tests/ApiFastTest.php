59863680687057a49960cdac13d74c2d17fefaae
avengerweb <avengerweb@yandex.ru>
2016-07-07 17:11:20 +0300

    Remove example test Tab => space

--- tests/ApiFastTest.php
+++ tests/ApiFastTest.php
@@ -2,39 +2,39 @@
 
 class ApiFastTest extends TestCase
 {
-	protected $baseUrl = '';
+    protected $baseUrl = '';
 
-	/**
-	 * ApiFastTest constructor.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * ApiFastTest constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testEmotes()
-	{
-		// WoTlk
-		$this->json('GET', '/api/v1/dbc/emotes/')->seeJson(
-			["id" => 0, "emote" => "ONESHOT_NONE"]
-		);
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testEmotes()
+    {
+        // WoTlk
+        $this->json('GET', '/api/v1/dbc/emotes/')->seeJson(
+            ["id" => 0, "emote" => "ONESHOT_NONE"]
+        );
 
-		$this->json('GET', '/api/v1/dbc/emotes/3')->seeJsonEquals(
-			["id" => 3, "emote" => "ONESHOT_WAVE(DNR)"]
-		);
+        $this->json('GET', '/api/v1/dbc/emotes/3')->seeJsonEquals(
+            ["id" => 3, "emote" => "ONESHOT_WAVE(DNR)"]
+        );
 
-		// Wod
-		$this->json('GET', '/api/v1/dbc/emotes/?version=wod')->seeJson(
-			["id" => 577, "emote" => "ONESHOT_ATTACK1H"]
-		);
+        // Wod
+        $this->json('GET', '/api/v1/dbc/emotes/?version=wod')->seeJson(
+            ["id" => 577, "emote" => "ONESHOT_ATTACK1H"]
+        );
 
-		$this->json('GET', '/api/v1/dbc/emotes/619/?version=wod')->seeJsonEquals(
-			["id" => 619, "emote" => "STATE_PARRY_UNARMED"]
-		);
-	}
+        $this->json('GET', '/api/v1/dbc/emotes/619/?version=wod')->seeJsonEquals(
+            ["id" => 619, "emote" => "STATE_PARRY_UNARMED"]
+        );
+    }
 }
